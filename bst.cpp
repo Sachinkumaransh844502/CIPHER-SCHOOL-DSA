6b68ac2fc10fe960ef449add6fd8320ac606558e
Sachin Kumar Ansh <120173047+Sachinkumaransh844502@users.noreply.github.com>
2024-09-13 14:27:29 +0530

    bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -1,175 +1,146 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-class Node{
-public:
-    int key;
-    Node* left;
-    Node* right;
-
-    Node(int key){
-        this->key=key;
-        left=right=NULL;
+struct Node{
+    int data;
+    Node *left,*right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
     }
 };
 
-
-Node* insert(Node* root,int key){
-    if(root==NULL){     // if empty then create one
-        return new Node(key);
+Node* insertBST(Node *root ,int val){
+    if(root==NULL){
+        return new Node(val);
     }
-    if(key<root->key){      // if root>key then use left
-        root->left=insert(root->left,key);
-    }
-    else{       // if root<key then use right
-        root->right=insert(root->right,key);
+    if(val<root->data){
+        root->left=insertBST(root->left,val);
+    }else{
+        root->right=insertBST(root->right,val);
     }
     return root;
 }
 
-
-void inOrder(Node* root){       //left key right
+void inorder(Node *root){
     if(root==NULL){
         return;
     }
-    inOrder(root->left);
-    cout<<root->key<<" ";
-    inOrder(root->right);
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
 }
 
-void preOrder(Node* root){      //key left right
-    if(root==NULL){
-        return;
-    }
-    cout<<root->key<<" ";
-    preOrder(root->left);
-    preOrder(root->right);
-}
-
-void postOrder(Node* root){     //left right key
-    if(root==NULL){
-        return;
-    }
-    postOrder(root->left);
-    postOrder(root->right);
-    cout<<root->key<<" ";
-}
-
-int sumOfNodes(Node* root) {
-    if (root == nullptr) {
-        return 0;
-    }
-
-    return root->key + sumOfNodes(root->left) + sumOfNodes(root->right);
-}
-
-bool search(Node* root,int key){
-    if(root==NULL){
-        return false;
-    }
-    if(root->key==key){
-        return true;
-    }
-    if(key>root->key){
-        return search(root->left,key);
-    }
-    return search(root->right,key);
-}
-
-Node* findMin(Node* root){
-    while(root->left!=NULL){        //root->left coz left has small values than root
-        root=root->left;
-    }
-    return root;        // returns smallest root node
-}
-
-
-Node* remove(Node* root,int key){
+Node* searchBST(Node *root,int key){
     if(root==NULL){
         return NULL;
     }
-    // To Find the Key 
-    else if(key<root->key){
-        root->left=remove(root->left,key);
+
+    if(root->data==key){
+        return root;
     }
-    else if(key>root->key){
-        root->right=remove(root->right,key);
+    if(root->data>key){
+        return searchBST(root->left,key);
+    }
+    return searchBST(root->right,key);
+}
+void search(Node *root){
+    int x;
+    cout<<"Enter the key to search : ";
+    cin>>x;
+    if(searchBST(root,x)==NULL){
+        cout<<"Key Doesn't Exists !!"<<endl;
+    }else{
+        cout<<"Key Exists !!"<<endl;
+    }
+}
+Node* inorderSucc(Node *root){
+    Node* c=root;
+    while(c && c->left!=NULL){
+        c=c->left;
+    }
+    return c;
+}
+Node* deleteBST(Node *root,int key){
+    if(root==NULL){
+        cout<<"Key "<<key<<" was not found in the BST."<<endl;
+        return root;
+    }
+    if(key<root->data){
+        root->left=deleteBST(root->left,key);
+    }
+    else if(key>root->data){
+        root->right=deleteBST(root->right,key);
     }
     else{
-        // When the Current Node Is Equal to Key
-
-        // No Children
-        if(root->left==NULL and root->right==NULL){
-            delete root;
-            root=NULL;
+        if(root->left == NULL){
+            Node *temp=root->right;
+            free(root);
+            return temp;
         }
-        // One Childern
-        else if(root->left==NULL){
-            Node* temp=root;
-            root=root->right;
-            delete temp;
+        else if(root->right == NULL){
+            Node *temp=root->left;
+            free(root);
+            return temp;
         }
-        else if(root->right==NULL){
-            Node* temp=root;
-            root=root->left;
-            delete temp;
-        }
-        // Two Children
-        else{
-            Node*temp=findMin(root->right);     //finding inorder successor from right subtree
-
-            root->key=temp->key;        // copying inorder successor to key node
-
-            root->right=remove(root->right,temp->key);      // removing temp->key (temp id inorder succesor ) and the whole subtree is to right of root
-        }
-
-    return root;
-
+        Node *temp = inorderSucc(root->right);
+        root->data = temp->data;
+        root->right = deleteBST(root->right,temp->data);
     }
-
+    return root;
 }
 
+int main(){
+    Node *root=NULL;
+    //Insertion
+    int i=1;
+    while (i!=0)
+    {
+        cout<<"Enter a Node of BST : "<<endl;
+        int f;
+        cin>>f;
+        if(root==NULL)
+        {
+        root=insertBST(root,f);
+        }
 
-int main()
-{
-    Node* root=NULL;
-    int arr[]={8,3,10,1,6,14,4,7,13};
-    for(int x:arr){     // traversing through array
-        root=insert(root,x);
+        insertBST(root,f);
+        cout<<"Do you want to continue(Yes->1,No->0)? : "<<endl;
+        cin>>i;
     }
-    cout<<"Inorder Traversal gives :";
-    inOrder(root);
-    cout<<endl;
-
-    cout<<"PostOrder Traversal gives :";
-    postOrder(root);
-    cout<<endl;
-    
-    cout<<"PreOrder Traversal gives :";
-    preOrder(root);
-    cout<<endl;
-    //Searching
-    int key;
-    cout<<"Enter what you want to search :";
-    cin>>key;
-    cout<<endl;
-    int ans=search(root,key);
-    if(ans==1){
-        cout<<"Key is present in Tree"<<endl;
+    i=1;
+    while(i!=0){
+        cout<<"Select Operations to Perform : "<<endl;
+        cout<<"1)Search\n2)Delete\n3)InOrder Form\n0)Exit "<<endl;
+        cout<<"Enter your choice : ";
+        int choice;
+        cin>>choice;
+        cout<<endl;
+        switch (choice)
+        {
+        case 0:
+            i=0;
+            break;
+        case 1:
+            search(root);
+            cout<<endl;
+            break;
+        case 2:
+            int del;
+            cout<<"Enter a Key to Delete : ";
+            cin>>del;
+            root=deleteBST(root,del);
+            cout<<endl;
+            break;
+        case 3:
+            cout<<"Inorder Form : ";
+            inorder(root);
+            cout<<"\n"<<endl;
+            break;
+        default:
+            cout<<"Invalid Choice!!\n"<<endl;
+            break;
+        }
     }
-    else{
-        cout<<"Key is not present in Tree"<<endl;
-    }
-    cout<<endl;
-
-    //Deletion
-    int key2;
-    cout<<"Enter what you want to delete :";
-    cin>>key2;
-    cout<<endl;
-    root=remove(root,key2);
-    cout<<endl;
-    cout<<"After deletion :";
-    inOrder(root);
-    cout<<endl;
     return 0;
 }
